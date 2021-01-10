4022936358f87f08a219a676bea8e5b2d282f87a
Bastien Silhol <bastien.silhol@epitech.eu>
2021-01-10 01:16:06 +0100

    Removed firebaseConfig

--- firebase/config.js
+++ firebase/config.js
@@ -1,13 +1,5 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCghos4M8HuaMoVruZv6j0Q-LQoWOM7i0c',
-  authDomain: 'happyfeet-fb45e.firebaseapp.com',
-  projectId: 'happyfeet-fb45e',
-  storageBucket: 'happyfeet-fb45e.appspot.com',
-  messagingSenderId: '144299611233',
-  appId: '1:144299611233:web:c839879b29cbac6b128620',
-  measurementId: 'G-4E8FEVCM4B',
-};
+const firebaseConfig = {};
 
 export default firebase.initializeApp(firebaseConfig);
